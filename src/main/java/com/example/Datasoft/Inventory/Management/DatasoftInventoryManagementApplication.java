b6d52b63fa049ed6acd9990c448393a82321516d
Rafsan <sending.to.evan@gmail.com>
2025-09-24 15:06:04 +0600

    Initial commit

---
+++ src/main/java/com/example/Datasoft/Inventory/Management/DatasoftInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.Datasoft.Inventory.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasoftInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello World");
+	}
+
+}
